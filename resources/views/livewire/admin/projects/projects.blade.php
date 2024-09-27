975bc5acd60a059370bef217049de43c44c4d636
qusairayan <qusairayan8@gmail.com>
2024-09-27 15:29:49 +0300

    csa

--- resources/views/livewire/admin/projects/projects.blade.php
+++ resources/views/livewire/admin/projects/projects.blade.php
@@ -69,9 +69,9 @@
                             </span>
                         </td>
                         <td>
-                            <a class="btn action-btn btn--danger btn-outline-danger p-1" href="{{ route('projects.delete', $project->id) }}" title="delete offer">
+                            <a class="btn action-btn btn--danger btn-outline-danger py-1 px-2" href="{{ route('projects.delete', $project->id) }}" title="delete offer">
                                 <i>
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" width="20" height="20" fill="currentColor">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor">
     <path d="M3 6l3 0 0-1c0-0.552 0.448-1 1-1h8c0.552 0 1 0.448 1 1l0 1 3 0c0.552 0 1 0.448 1 1s-0.448 1-1 1l-1 0 0 12c0 1.656-1.344 3-3 3h-8c-1.656 0-3-1.344-3-3l0-12-1 0c-0.552 0-1-0.448-1-1s0.448-1 1-1zm5-1l0 1 6 0 0-1h-6zm9 3l0 12c0 0.552-0.448 1-1 1h-8c-0.552 0-1-0.448-1-1l0-12 10 0z"></path>
   </svg>
 </i>
